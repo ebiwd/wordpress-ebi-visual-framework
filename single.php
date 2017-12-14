f929c032be4357a6e8fd64fcb3e1a65b55d8cdeb
khawkins98 <khawkins98@gmail.com>
2017-12-14 10:11:04 +0000

    Clean up header formatting

--- single.php
+++ single.php
@@ -22,7 +22,7 @@ get_header(); ?>
   	);
   ?>
   <article <?php post_class($mainClasses) ?> id="post-<?php the_ID(); ?>">
-    <header>
+    <header class="clearfix padding-bottom-medium">
       <h2 class="entry-title"><?php the_title(); ?></h2>
       <?php ebiframework_entry_meta(); ?>
     </header>
